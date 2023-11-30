41dde09a9b0ef718c94b758f74d2b3d3a240c9cc
Jan Šuráň <suran.orgpad@gmail.com>
2023-11-30 21:16:21 +0100

    Improve application parsing, add more predefines lambda expressions.

--- semestral/src/semestral/core.clj
+++ semestral/src/semestral/core.clj
@@ -1,17 +1,22 @@
 (ns semestral.core
-  (:import (java.io Writer)))
+  (:require [clojure.string :as str])
+  (:import (clojure.lang Keyword)
+           (java.io Writer)))
 
 (defprotocol StringConvertible
   (as-str [this]))
 
 (defrecord Lambda [arg body])
 
-(defn lambda [& args]
+(defn- parse-symbols [s]
+  (str/split s #" "))
+
+(defn lambda [args body]
   ((fn expand [[arg & more]]
      (if more
        (Lambda. arg (expand more))
        arg))
-   args))
+   (conj (parse-symbols args) body)))
 
 (def l lambda)
 
@@ -19,9 +24,16 @@
 
 (defn application [& args]
   ((fn expand [[arg & more]]
-     (if more
-       (Application. arg (expand more))
-       arg))
+     (cond (string? arg) (let [[sym1 sym2 & syms] (parse-symbols arg)]
+                           (cond sym2 (let [first-two (Application. sym1 sym2) ; first two are symbols
+                                            more (concat syms more)]
+                                        (if (seq more)
+                                          (Application. first-two (expand more))
+                                          first-two))
+                                 more (Application. sym1 (expand more)) ; first is symbol, second is call
+                                 :else sym1))               ; only 1 symbol
+           more (Application. arg (expand more))
+           :else arg))
    args))
 
 (def call application)
@@ -31,6 +43,8 @@
   (as-str [this] this)
   Character
   (as-str [this] (str this))
+  Keyword
+  (as-str [this] (name this))
   Lambda
   (as-str [{:keys [arg body]}]
     (str "(λ " (as-str arg) " . " (as-str body) ")"))
@@ -42,8 +56,21 @@
   [lambda ^Writer w]
   (.write w ^String (as-str lambda)))
 
-(def T (l "t" "f" "t"))
-(def F (l "t" "f" "f" ))
-(def Z (l "s" "s" (call "s" "z")))
-(def Y (l "f" (call (l "x" (call "f" (call "x" "x")))
-                    (l "x" (call "f" (call "x" "x"))))))
+(def TRUE (l "t f" "t"))
+(def FALSE (l "t f" "f"))
+(def ZERO (l "s z" (call "s z")))
+(def Y-COMB (l "f" (call (l "x" (call "f" (call "x x")))
+                         (l "x" (call "f" (call "x x"))))))
+(def ADD (l "x y s z" (call "x s" (call "y s z"))))
+(def AND (l "x y" (call "x y x")))
+(def OR (l "x y" (call "x x y")))
+(def NOT (l "x y" (call "x" FALSE TRUE)))
+(def MUL (l "x y s" (call "x" (call "y s"))))
+(def NAND (l "x y" (call (call "x y x")
+                    FALSE
+                    TRUE)))
+
+(defmacro sym-map [& syms]
+  (into {} (map #(vector (str %) %)) syms))
+
+(def predefs (sym-map TRUE FALSE ZERO Y-COMB ADD AND OR NOT MUL NAND))
